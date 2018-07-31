db4942931f933bc79020f0fac5fbe18ed6fdbc1c
王昊峰 <wanghaofeng@wanghaofengdeMacBook-Pro.local>
2018-07-31 18:23:45 +0800

    未完成的题目更新

--- Leetcode6.py
+++ Leetcode6.py
@@ -6,7 +6,7 @@ And then read line by line: "PAHNAPLSIIGYIR"
 Write the code that will take a string and make this conversion given a number of rows:
 string convert(string s, int numRows);
 '''
-string convert(string s, int numRows);
+# string convert(string s, int numRows);
 class Solution:
     def convert(self, s, numRows):
         lst = [ ['0' for i in range(len(s) // 2 + 1)] for j in range(numRows) ]
